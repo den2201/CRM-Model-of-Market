75f381c521ff4e148d124a4a3fd491318c14f288
Denis N <service4tv@gmail.com>
2019-12-24 22:34:29 +0300

    Add project files.

---
+++ CrmBL/Model/Seller.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrmBL.Model
+{
+    class Seller
+    {
+        public int SellerID{get;set;}
+        public string Name { get; set; }
+
+    }
+}
